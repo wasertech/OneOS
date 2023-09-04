a1b905c070daf6e3189331ddbf82c5e66fe15a89
wasertech <danny@waser.tech>
2023-09-04 16:21:32 +0200

    push dataset to hub

--- data/nl2shell/__main__.py
+++ data/nl2shell/__main__.py
@@ -1,10 +1,20 @@
 
 from nl2shell import LANGS
 from nl2shell.assistant import get_assistant_text_data
+from datasets import Dataset
 
 if __name__ == "__main__":
     
     text_data = get_assistant_text_data()
+
+    dataset_dict = {"text": text_data}
+
+    # Create the Hugging Face dataset
+    dataset = Dataset.from_dict(dataset_dict)
+
+    # Push to the hub
+    dataset.push_to_hub("OneOS")
+
 
     # for text_data in text_data:
     #     print(text_data)
