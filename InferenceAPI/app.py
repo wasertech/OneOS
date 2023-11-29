658b0e19093ed3b3b3eac9991de910c9b6a12591
wasertech <danny@waser.tech>
2023-11-29 23:54:25 +0100

    fix embeddings response

--- InferenceAPI/app.py
+++ InferenceAPI/app.py
@@ -68,7 +68,7 @@ async def emmbed(request: Request) -> Response:
     final_output = results_generator.tolist()[0]
 
     assert final_output
-    ret = {"embedings": final_output}
+    ret = {"embeddings": final_output}
     return JSONResponse(ret)
 
 @app.post("/generate")
