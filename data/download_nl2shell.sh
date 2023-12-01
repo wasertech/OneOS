7ec0d8257cace905259316f6859c22811af1fddf
wasertech <danny@waser.tech>
2023-12-01 20:23:48 +0100

    update nl2shell download script

--- data/download_nl2shell.sh
+++ data/download_nl2shell.sh
@@ -1,2 +1,25 @@
-curl -o nl2shell.nl https://raw.githubusercontent.com/TellinaTool/nl2bash/master/data/bash/all.nl
-curl -o nl2shell.cm https://raw.githubusercontent.com/TellinaTool/nl2bash/master/data/bash/all.cm
+#!/usr/bin/env bash
+
+function download_file {
+    fileuri=$1
+    filename=$2
+    curl -o "${filename}" "${fileuri}"
+}
+
+base_uri="https://raw.githubusercontent.com/TellinaTool/nl2bash/master/data/bash/"
+
+f="nl2shell.en.nl"
+if [ -f ${f} ]; then
+    echo "File ${f} already exist"
+else
+    echo "Downloading ${f}..."
+    download_file "${base_uri}/all.nl" "${f}"
+fi
+
+f="nl2shell.cm"
+if [ -f ${f} ]; then
+    echo "File ${f} already exist"
+else
+    echo "Downloading ${f}..."
+    download_file "${base_uri}/all.cm" "${f}"
+fi
