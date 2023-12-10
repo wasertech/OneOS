4aa3576976480c8a185cdc5f840367e29bee4de4
wasertech <danny@waser.tech>
2023-12-10 19:00:57 +0100

    switched inference to awq

--- InferenceAPI/app.sh
+++ InferenceAPI/app.sh
@@ -5,12 +5,13 @@ set -xe
 
 HOST=${HOST:-0.0.0.0}
 PORT=${PORT:-5085}
-MODEL_ID=${MODEL_ID:-'TheBloke/Llama-2-7b-chat-fp16'}
-TOKENIZER_ID=${TOKENIZER_ID:-'hf-internal-testing/llama-tokenizer'}
-DTYPE=${DTYPE:-'auto'} # 'auto', 'half', 'float', 'bfloat16'
-QUANT=${QUANT} # None, 'awq'
+MODEL_ID=${MODEL_ID:-'TheBloke/dolphin-2.1-mistral-7B-AW'}
+TOKENIZER_ID=${TOKENIZER_ID:-MODEL_ID}
+DTYPE=${DTYPE:-'half'} # 'auto', 'half', 'float', 'bfloat16'
+QUANT=${QUANT:-'awq'} # None, 'awq'
 N_GPUS=${N_GPUS:-1}
-GPU_MEM=${GPU_MEM:-0.9}
+GPU_MEM=${GPU_MEM:-0.45}
+MAX_LENGTH=${MAX_LENGTH:-8192}
 
 QUANT_FLAG=""
 
@@ -19,7 +20,12 @@ if [ -n "$QUANT" ]; then
     QUANT_FLAG="--quantization ${QUANT}"
 fi
 
-python app.py --host $HOST --port $PORT --model $MODEL_ID --tokenizer $TOKENIZER_ID --dtype $DTYPE --tensor-parallel-size $N_GPUS --gpu-memory-utilization $GPU_MEM $QUANT_FLAG
+python app.py \
+--host $HOST --port $PORT \
+--model $MODEL_ID --tokenizer $TOKENIZER_ID \
+--dtype $DTYPE --max-model-len $MAX_LENGTH \
+--tensor-parallel-size $N_GPUS --gpu-memory-utilization $GPU_MEM \
+$QUANT_FLAG
 
 # Uncomment to run the OpenAI API server (comment out the line above)
 # pydantic==1.10.13
